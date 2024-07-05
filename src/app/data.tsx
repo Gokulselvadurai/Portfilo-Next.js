83b3f6d64c862b4ef9625cb497b1b1cb6f4139d6
Gokul S <101489062+Gokulselvadurai@users.noreply.github.com>
2024-07-05 13:22:07 +0530

    Update data.tsx

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -13,14 +13,14 @@ interface Product {
     },
     {
       id: 2,
-      img: "https://images.unsplash.com/photo-1559819614-81fea9efd090?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXJkdWlub3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-      link: "https://github.com/Gokulselvadurai/Gas-Leak-Detection-And-Fire-Alert-System",
-      title: "Gas Leak And Fire Alert System",
-    },
-    {
-      id: 3,
       img: "https://images.unsplash.com/photo-1633356122544-f134324a6cee?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       link: "https://github.com/Gokulselvadurai/Stack-Overflow-clone",
       title: "Stack Overflow clone",
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1559819614-81fea9efd090?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXJkdWlub3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      link: "https://github.com/Gokulselvadurai/Gas-Leak-Detection-And-Fire-Alert-System",
+      title: "Gas Leak And Fire Alert System",
     },
   ];
